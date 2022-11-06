886788e2929aed3cd3d561b937e850709e296f9f
amirbigg <amirbig44@gmail.com>
2022-11-06 20:34:38 +0330

    add ep8

---
+++ tracking/app_settings.py
@@ -0,0 +1,14 @@
+class AppSettings:
+	def __init__(self, prefix):
+		self.prefix = prefix
+
+	def _setting(self, name, dflt):
+		from django.conf import settings
+
+		return getattr(settings, self.prefix + name, dflt)
+
+	@property
+	def PATH_LENGTH(self):
+		return self._setting('PATH_LENGTH', 200)
+
+app_settings = AppSettings('DRF_TRACKING_')
--- tracking/base_mixins.py
+++ tracking/base_mixins.py
@@ -1,5 +1,6 @@
 from django.utils.timezone import now
 import ipaddress
+from .app_settings import app_settings
 
 
 class BaseLoggingMixin:
@@ -9,8 +10,18 @@ class BaseLoggingMixin:
 
 	def finalize_response(self, request, response, *args, **kwargs):
 		response = super().finalize_response(request, response, *args, **kwargs)
+		user = self._get_user(request)
 		self.log.update({
 			'remote_addr': self._get_ip_address(request),
+			'view': self._get_view_name(request),
+			'view_method': self._get_view_method(request),
+			'path': self._get_path(request),
+			'host': request.get_host(),
+			'method': request.method,
+			'user': user,
+			'username_persistent': user.get_username() if user else 'Anonymous',
+			'response_ms': self._get_response_ms(),
+			'status_code': response.status_code,
 		})
 		self.handle_log()
 		return response
@@ -34,3 +45,31 @@ class BaseLoggingMixin:
 				pass
 
 		return ipaddr
+
+	def _get_view_name(self, request):
+		method = request.method.lower()
+		try:
+			attribute = getattr(self, method)
+			return (type(attribute.__self__).__module__ + "." + type(attribute.__self__).__name__)
+		except AttributeError:
+			return None
+
+	def _get_view_method(self, request):
+		if hasattr(self, 'action'):
+			return self.action or None
+		return request.method.lower()
+
+	def _get_path(self, request):
+		return request.path[:app_settings.PATH_LENGTH]
+
+	def _get_user(self, request):
+		user = request.user
+		if user.is_anonymous:
+			return None
+		return user
+
+	def _get_response_ms(self):
+		response_timedelta = now() - self.log['requested_at']
+		response_ms = int(response_timedelta.total_seconds() * 1000)
+		return max(response_ms, 0)
+
