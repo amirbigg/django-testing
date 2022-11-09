99a301bf8ee1776a816702be722664d49587b867
amirbigg <amirbig44@gmail.com>
2022-11-09 10:48:32 +0330

    add ep10

--- tracking/base_mixins.py
+++ tracking/base_mixins.py
@@ -1,11 +1,17 @@
 from django.utils.timezone import now
 import ipaddress
 import traceback
+import logging
+import ast
 from .app_settings import app_settings
 
 
+logger = logging.getLogger(__name__)
+
 class BaseLoggingMixin:
 	logging_methods = '__all__'
+	sensitive_fields = {}
+	CLEANED_SUBSTITUTE = '*****************'
 
 	def initial(self, request, *args, **kwargs):
 		self.log = {'requested_at':now()}
@@ -20,6 +26,12 @@ class BaseLoggingMixin:
 		response = super().finalize_response(request, response, *args, **kwargs)
 		if self.should_log(request, response):
 			user = self._get_user(request)
+			if response.streaming:
+				rendered_content = None
+			elif hasattr(response, 'rendered_content'):
+				rendered_content = response.rendered_content
+			else:
+				rendered_content = response.getvalue()
 			self.log.update({
 				'remote_addr': self._get_ip_address(request),
 				'view': self._get_view_name(request),
@@ -31,8 +43,13 @@ class BaseLoggingMixin:
 				'username_persistent': user.get_username() if user else 'Anonymous',
 				'response_ms': self._get_response_ms(),
 				'status_code': response.status_code,
+				'query_params': self._clean_data(request.query_params.dict()),
+				'response': self._clean_data(rendered_content)
 			})
-			self.handle_log()
+			try:
+				self.handle_log()
+			except Exception:
+				logger.exception('Logging API call raise exception!')
 		return response
 
 	def handle_log(self):
@@ -86,3 +103,25 @@ class BaseLoggingMixin:
 		return (
 			self.logging_methods == '__all__' or request.method in self.logging_methods
 		)
+
+	def _clean_data(self, data):
+
+		if isinstance(data, list):
+			return [self._clean_data(d) for d in data]
+
+		if isinstance(data, dict):
+			SENSITIVE_FIELDS = {'api', 'token', 'key', 'secret', 'password', 'signature'}
+			if self.sensitive_fields:
+				SENSITIVE_FIELDS = SENSITIVE_FIELDS | {field.lower() for field in self.sensitive_fields}
+
+				for key, value in data.items():
+					try:
+						value = ast.literal_eval(value)
+					except (ValueError, SyntaxError):
+						pass
+
+					if isinstance(value, (list, dict)):
+						data[key] = self._clean_data(value)
+					if key.lower() in SENSITIVE_FIELDS:
+						data[key] = self.CLEANED_SUBSTITUTE
+		return data
--- tracking/views.py
+++ tracking/views.py
@@ -4,5 +4,6 @@ from .mixins import LoggingMixin
 
 
 class Home(LoggingMixin, APIView):
+	sensitive_fields = {'pass'}
 	def get(self, request):
 		return Response('hello')
